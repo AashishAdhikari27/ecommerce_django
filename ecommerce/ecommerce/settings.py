b013f3eeef88a3346deb070aff1240bee50cfe56
Aashish <aashishadhikari2027@gmailcom>
2024-05-22 02:22:25 +0545

    products and their images rendered

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -122,6 +122,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+MEDIA_URL = '/images/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -129,3 +130,4 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
